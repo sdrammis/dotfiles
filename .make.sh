a6968c9ddafd1d7f01a31ef7af4805fb7f5f779a
sdrammis <sdrammis@mit.edu>
2019-05-16 21:19:50 -0400

    Update install script

--- .make.sh
+++ .make.sh
@@ -4,7 +4,7 @@
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
-
+dir=~/dotfiles
 olddir=~/dotfiles_old # old dotfiles backup directory
 files="bashrc vimrc vim zshrc oh-my-zsh" # list of files to symlink in homedir
 codeFiles="keybindings.json settings.json spellright.dict" # VSCode configs
@@ -15,28 +15,26 @@ mkdir -p $olddir
 echo "...done"
 echo "Backing up old files... to $olddir"
 for file in $files; do
-    mv ~/.$file ~/dotfiles_old/$file
+    mv ~/.$file $olddir/$file
 done
+mkdir -p $olddir/code
 for file in $codeFiles; do
-    mv ~/.config/Code/User/$file ~/dotfiles_old/code/$file
+    mv ~/.config/Code/User/$file $olddir/code/$file
 done
 mv ~/.config/i3/config ~/dotfiles_old/i3config
 echo "...done"
 
 
 ## Symlink new configs
-# echo "Linking new files... to ~"
-# for file in $files; do
-#     echo "Creating symlink to $file in ~"
-#     ln -s ./$file ~/.$file
-# done
-# for file in $codeFiles; do
-#     echo "Creating symlink to $file in ~"
-#     ln ./code/$file ~/.config/Code/User/$file
-# done
-# echo "Creating symlink to i3 config in ~"
-# ln ./i3config ~/.config/i3/config
-# echo "...done"
-
-# # Install VSCode Plugins
-# ./code/install.sh
+echo "Linking new files... to ~"
+for file in $files; do
+    echo "Creating symlink to $file in ~"
+    ln -s $dir/$file ~/.$file
+done
+for file in $codeFiles; do
+    echo "Creating symlink to $file in ~"
+    ln $dir/code/$file ~/.config/Code/User/$file
+done
+echo "Creating symlink to i3 config in ~"
+ln $dir/i3config ~/.config/i3/config
+echo "...done"
